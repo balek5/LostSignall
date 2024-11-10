9434002177e04fa9824805cb41d3491ca6224f44
balek5 <90638569+balek5@users.noreply.github.com>
2024-11-10 13:21:58 +0100

    Add files via upload

---
+++ Spacewebsitebalek/LostSingal/Space Connect/JavaScript/index.js
@@ -0,0 +1,12 @@
+/* ############## Landing Page Parallax Effect ############### */
+
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+
+
+let cart_btn = document.querySelector('#cart-btn');
+let cart = document.querySelector('#cart');
+
+cart_btn.addEventListener('click', () => {
+    cart.style.display == 'block' ? cart.style.display = 'none' : cart.style.display = 'block';
+})
